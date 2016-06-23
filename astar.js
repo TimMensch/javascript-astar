d466c7e1078ea2a136fdd537fa65c88f99ab0cc8
Kit Burgess <kburgess@gamedesignkit.net>
2016-06-22 19:33:32 -0600

    WIP Reworking pathfinding to use a hexagonal coordinate system;

--- astar.js
+++ astar.js
@@ -42,13 +42,11 @@ var astar = {
   * @param {Object} [options]
   * @param {bool} [options.closest] Specifies whether to return the
              path to the closest node if the target is unreachable.
-  * @param {Function} [options.heuristic] Heuristic function (see
-  *          astar.heuristics).
   */
   search: function(graph, start, end, options) {
     graph.cleanDirty();
     options = options || {};
-    var heuristic = options.heuristic || astar.heuristics.manhattan;
+    var heuristic = astar.heuristics.hexagonal;
     var closest = options.closest || false;
 
     var openHeap = getHeap();
@@ -136,6 +134,11 @@ var astar = {
       var d1 = Math.abs(pos1.x - pos0.x);
       var d2 = Math.abs(pos1.y - pos0.y);
       return (D * (d1 + d2)) + ((D2 - (2 * D)) * Math.min(d1, d2));
+    },
+    hexagonal: function (pos0, pos1) {
+      cube0 = AxialToCube(pos0);
+      cube1 = AxialToCube(pos1);
+      return Math.max(Math.abs(cube0.x - cube1.x), Math.abs(cube0.y - cube1.y), Mathf.abs(cube0.z - cube1.z));
     }
   },
   cleanNode: function(node) {
@@ -148,16 +151,29 @@ var astar = {
   }
 };
 
+function AxialToCube(axialPos) {
+  var cube = {};
+  cube.x = axialPos.x;
+  cube.z = axialPos.y;
+  cube.y = -cube.x - cube.z;
+  return cube;
+}
+
+function CubeToAxial(cubePos) {
+  var axial = {};
+  axial.x = cubePos.x;
+  axial.y = cubePos.z;
+}
+
 /**
  * A graph memory structure
  * @param {Array} gridIn 2D array of input weights
  * @param {Object} [options]
- * @param {bool} [options.diagonal] Specifies whether diagonal moves are allowed
  */
 function Graph(gridIn, options) {
   options = options || {};
   this.nodes = [];
-  this.diagonal = !!options.diagonal;
+  //this.diagonal = !!options.diagonal;
   this.grid = [];
   for (var x = 0; x < gridIn.length; x++) {
     this.grid[x] = [];
@@ -195,9 +211,15 @@ Graph.prototype.neighbors = function(node) {
   var y = node.y;
   var grid = this.grid;
 
-  // West
-  if (grid[x - 1] && grid[x - 1][y]) {
-    ret.push(grid[x - 1][y]);
+
+  // // North
+  // if (grid[x] && grid[x][y + 1]) {
+  //   ret.push(grid[x][y + 1]);
+  // }
+
+  // NorthEast
+  if (grid[ x + 1 ] && grid[ x + 1 ][ y - 1 ]) {
+    ret.push(grid[ x + 1 ][ y - 1 ]);
   }
 
   // East
@@ -205,36 +227,29 @@ Graph.prototype.neighbors = function(node) {
     ret.push(grid[x + 1][y]);
   }
 
-  // South
-  if (grid[x] && grid[x][y - 1]) {
-    ret.push(grid[x][y - 1]);
+  // SouthEast
+  if (grid[ x ] && grid[ x ][ y + 1 ]) {
+    ret.push(grid[ x ][ y + 1 ]);
   }
 
-  // North
-  if (grid[x] && grid[x][y + 1]) {
-    ret.push(grid[x][y + 1]);
+  // // South
+  // if (grid[x] && grid[x][y - 1]) {
+  //   ret.push(grid[x][y - 1]);
+  // }
+
+  // SouthWest
+    if (grid[ x - 1 ] && grid[ x - 1 ][ y + 1 ]) {
+    ret.push(grid[ x - 1 ][ y + 1 ]);
   }
 
-  if (this.diagonal) {
-    // Southwest
-    if (grid[x - 1] && grid[x - 1][y - 1]) {
-      ret.push(grid[x - 1][y - 1]);
-    }
+  // West
+  if (grid[x - 1] && grid[x - 1][y]) {
+    ret.push(grid[x - 1][y]);
+  }
 
-    // Southeast
-    if (grid[x + 1] && grid[x + 1][y - 1]) {
-      ret.push(grid[x + 1][y - 1]);
-    }
-
-    // Northwest
-    if (grid[x - 1] && grid[x - 1][y + 1]) {
-      ret.push(grid[x - 1][y + 1]);
-    }
-
-    // Northeast
-    if (grid[x + 1] && grid[x + 1][y + 1]) {
-      ret.push(grid[x + 1][y + 1]);
-    }
+  // NorthWest
+    if (grid[ x ] && grid[ x ][ y - 1 ]) {
+    ret.push(grid[ x ][ y - 1 ]);
   }
 
   return ret;
